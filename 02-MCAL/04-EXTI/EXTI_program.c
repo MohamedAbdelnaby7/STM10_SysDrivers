79ce23426b0d162c6e95841e6a0d0c52655aed43
MohamedAbdelnaby7 <43073043+MohamedAbdelnaby7@users.noreply.github.com>
2020-09-10 12:30:12 +0200

    EXTI driver added

--- 02-MCAL/04-EXTI/EXTI_program.c
+++ 02-MCAL/04-EXTI/EXTI_program.c
@@ -1,7 +1,7 @@
 /********************************************************************/
 /* Author	: MOHAMED ABDELNABY								 	 	*/
-/* Date		: 22 Aug 2020                                           */
-/* Version	: V01                                                	*/
+/* Date		: 31 Aug 2020                                           */
+/* Version	: V02                                                	*/
 /********************************************************************/
 
 #include "STD_TYPES.h"
@@ -11,8 +11,13 @@
 #include "EXTI_config.h"
 #include "EXTI_private.h"
 
+/*array of pointer to function*/
+static void(* EXTI_CallBack[16])(void);
+
 void MEXTI_voidInit()
 {
+	/*configuaring default external interrupt EXTI0 Rising edge*/
+	SET_BIT(EXTI -> IMR, EXTI_LINE);
 	#if EXTI_MODE == RISING
 		SET_BIT(EXTI -> RTSR, EXTI_LINE);
 	#elif EXTI_MODE == FALLING 
@@ -23,31 +28,99 @@ void MEXTI_voidInit()
 	#else
 		#error "Wrong Interrupt mode"
 	#endif
-	/*make sure the interrupt is diable at first*/
-	CLR_BIT(EXTI -> IMR, copy_u8Line);
 }
 
 
-void MEXIT_voidEnableEXTI(u8 copy_u8Line)
+void MEXTI_voidEnableEXTI(u8 Copy_u8Line)
 {
-	SET_BIT(EXTI -> IMR, copy_u8Line);
+	/*Enable external interrupt*/
+	SET_BIT(EXTI -> IMR, Copy_u8Line);
 }
 
-void MEXIT_voidDisableEXTI(u8 copy_u8Line)
+void MEXTI_voidDisableEXTI(u8 Copy_u8Line)
 {
-	CLR_BIT(EXTI -> IMR, copy_u8Line);
+	/*Disable external interrupt*/
+	CLR_BIT(EXTI -> IMR, Copy_u8Line);
 }
 
-void MEXIT_voidSwTrigger(u8 copy_u8Line)
+void MEXTI_voidSoftWareTrigger(u8 Copy_u8Line)
 {
-	SET_BIT(EXTI -> SWIER, copy_u8Line);
+	/*Enable SW interrupt*/
+	SET_BIT(EXTI -> SWIER, Copy_u8Line);
 }
 
-void MEXIT_voidSetSignalLatch(u8 copy_u8Line, u8 copy_u8Mode)
+void MEXTI_voidSetSignallatch(u8 Copy_u8Line, u8 Copy_u8SenseMode)
 {
-	case RISING:	SET_BIT(EXTI -> RTSR, EXTI_LINE); 	break;
-	case FALLING:	SET_BIT(EXTI -> FTSR, EXTI_LINE);	break;
-	case CHANGING:	SET_BIT(EXTI -> RTSR, EXTI_LINE); 
-					SET_BIT(EXTI -> FTSR, EXTI_LINE);	break;
-	
+	/*configuring EXTI on line Copy_u8Line with sensing mode Copy_u8SenseMode*/
+	switch(Copy_u8SenseMode)
+	{
+		case RISING:	SET_BIT(EXTI -> RTSR, Copy_u8Line); break;
+		case FALLING:	SET_BIT(EXTI -> FTSR, Copy_u8Line);	break;
+		case CHANGING:	SET_BIT(EXTI -> RTSR, Copy_u8Line); 
+						SET_BIT(EXTI -> FTSR, Copy_u8Line);	break;
+	}
+	MEXTI_voidEnableEXTI(Copy_u8Line);
+}
+
+void MEXTI_voidSetCallBack(void(*ptr)(void), u8 Copy_u8Line)
+{
+	/*set functions to call in the app layer when interrupt is fired*/
+	switch(Copy_u8Line)
+	{
+		case LINE0:
+			EXTI_CallBack[0] = ptr;
+			break;
+		case LINE1:
+			EXTI_CallBack[1] = ptr;
+			break;
+		case LINE2:
+			EXTI_CallBack[2] = ptr;
+			break;
+		case LINE3:
+			EXTI_CallBack[3] = ptr;
+			break;
+		case LINE4:
+			EXTI_CallBack[4] = ptr;
+			break;
+		case LINE5:
+			EXTI_CallBack[5] = ptr;
+			break;
+		case LINE6:
+			EXTI_CallBack[6] = ptr;
+			break;
+		case LINE7:
+			EXTI_CallBack[7] = ptr;
+			break;
+		case LINE8:
+			EXTI_CallBack[8] = ptr;
+			break;
+		case LINE9:
+			EXTI_CallBack[9] = ptr;
+			break;
+		case LINE10:
+			EXTI_CallBack[10] = ptr;
+			break;
+		case LINE11:
+			EXTI_CallBack[11] = ptr;
+			break;
+		case LINE12:
+			EXTI_CallBack[12] = ptr;
+			break;
+		case LINE13:
+			EXTI_CallBack[13] = ptr;
+			break;
+		case LINE14:
+			EXTI_CallBack[14] = ptr;
+			break;
+		case LINE15:
+			EXTI_CallBack[15] = ptr;
+			break;	
+	}
+}
+
+void EXTI0_IRQHandler(void)
+{
+	/*call function in EXTI_CallBack[0] when EXTI0 is fired*/
+	EXTI_CallBack[0]();
+	SET_BIT(EXTI -> PR, 0);
 }
